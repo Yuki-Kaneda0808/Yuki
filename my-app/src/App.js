274bf809f2db1d3220b94c4ad79249288ad27cc0
Yuki Kaneda <kaneda-y@biken.osaka-u.ac.jp>
2025-02-19 14:33:09 +0900

    Add my-app files

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Personal Website</h1>
+        <p>
+          これは私の研究や業績を紹介するためのサイトです。
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
